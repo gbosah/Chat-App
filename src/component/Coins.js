1e36f0db98f029b29eb6951c465a945a9f2bce98
Bosah <gbosah@gmail.com>
2022-04-26 20:51:49 +0100

    my crypto app

--- src/component/Coins.js
+++ src/component/Coins.js
@@ -5,7 +5,7 @@ function Coins(props) {
     return (
         <div className='relative p-2 text-justify w-screen bg-slate-400'>
              <div className='flex flex-row justify-around mt-10 space-x-10'>
-           <div className='flex'> {props.num} <img src={props.icon} width={24} className='mx-2'/> 
+           <div className='flex'> {props.num} <img src={props.icon} width={24} alt='crypto' className='mx-2'/> 
             {props.name} <span className='text-gray-400 mx-1'>{props.symbol}</span>
             </div>
            <div>{props.price}</div>
