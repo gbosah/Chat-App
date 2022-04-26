f9e01aad5d8f6cd50207b13f9ec77423e794b92e
Bosah <gbosah@gmail.com>
2022-04-26 20:23:50 +0100

    my crypto app

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, {useState, useEffect} from 'react';
+import Axios from 'axios';
+import Coins from './component/Coins';
+import { list } from 'postcss';
+import Navbar from './component/Navbar';
+
+function App() {
+  
+  const [crypto, setCrypto] = useState({coins:[]});
+  const [searchCrypto, setSearchCrypto] = useState('');
+
+  const filteredCoins = crypto.coins.filter(coin => {
+    return coin.name.toLowerCase().includes(searchCrypto.toLowerCase());
+  })
+
+  function handleChange(event) {
+    setSearchCrypto(event.target.value)
+  }
+
+  //fetch the api data using fetch
+  useEffect(() => {
+   fetch('https://api.coinstats.app/public/v1/coins?skip=0&')
+        .then(res => res.json())
+        .then(data => setCrypto(data))
+  }, []);
+  
+  const coinList = filteredCoins.map(lists => {
+    return <Coins name={lists.name}
+                  symbol={lists.symbol}
+                  num={lists.rank}
+                  price={lists.price.toLocaleString("en-US")}
+                  icon={lists.icon}
+                  hourChange={lists.priceChange1d}
+                  marketCap={lists.marketCap.toLocaleString("en-US")}
+                  availableSupply={lists.availableSupply.toLocaleString("en-US")}
+    />
+
+  })
+  return (
+    <div className='flex-grow'>
+      <Navbar handleChange={handleChange}/>
+      {coinList}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Navbar({handleChange}) {
+    return (
+        <div className='m-auto bg-green-300'>
+            <div className='flex justify-center mb-4'>
+            <input type='text' placeholder='search...' onChange={handleChange} className='mt-2 border-2 rounded-xl px-2 text-slate-400
+      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500'/>
+            </div>
+            <ul className='flex justify-around font-extrabold text-lg'>
+                <li>Name</li>
+                <li>Price</li>
+                <li>24h%</li>
+                <li>Market Cap</li>
+                <li>Circulating Supply</li>
+            </ul>     
+        </div>
+    );
+}
+
+export default Navbar;
