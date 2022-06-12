a53752cda47b6c80f463d9f40928c6232f528923
Bosah <gbosah@gmail.com>
2022-06-12 04:15:08 +0100

    crypto app

--- src/component/Coins.js
+++ src/component/Coins.js
@@ -3,9 +3,10 @@ import React from 'react';
 function Coins(props) {
 
     return (
-        <div className='relative p-2 text-justify bg-slate-400 md:w-screen sm:w-screen'>
-             <div className='flex flex-row justify-around mt-10 space-x-10 md:justify-around sm:justify-between'>
-           <div className='flex md:z-10 lg:z-0 sm:z-10'> {props.num} <img src={props.icon} width={24} alt='crypto' className='mx-2'/> 
+     <div className='w-screen p-2 bg-gray-700 text-white'>
+        <div className=' mt-10 space-x-10 grid grid-cols-5 md:grid-cols-5 '>
+           <div className='flex px-2'> 
+            {props.num} <span className='px-2'> <img src={props.icon} width={20} alt='crypto'/></span> 
             {props.name} <span className='text-gray-400 mx-1'>{props.symbol}</span>
             </div>
            <div>{props.price}</div>
@@ -13,7 +14,7 @@ function Coins(props) {
            <div>{props.marketCap}</div>
            <div>{props.availableSupply}</div>  
         </div>
-        </div>
+     </div>
     );
 }
 
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -2,18 +2,20 @@ import React from 'react';
 
 function Navbar({handleChange}) {
     return (
-        <div className='m-auto bg-green-300'>
+        <div className='w- flex flex-col bg-gray-700 text-white'>
             <div className='flex justify-center mb-4 '>
             <input type='text' placeholder='search...' onChange={handleChange} className='mt-2 border-2 rounded-xl px-2 text-slate-400
       focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500'/>
             </div>
-            <ul className='flex justify-around font-extrabold sm:mx-2 text-lg md:justify-around sm:justify-around'>
+            <div className='w-full'>
+            <ul className='flex justify-around font-extrabold text-lg'>
                 <li>Name</li>
                 <li>Price</li>
                 <li>24h%</li>
                 <li>Market Cap</li>
                 <li>Circulating Supply</li>
-            </ul>     
+            </ul>
+            </div>  
         </div>
     );
 }
